1b81e6aeb948cf9569970afdcde5ddeb8db94584
Nathan Andrus <nathanpandrus@gmail.com>
2019-09-04 10:22:54 -0600

    Fist commit- created tableview for journal

---
+++ GitHubJournal/JournalTableViewController.swift
@@ -0,0 +1,33 @@
+//
+//  JournalTableViewController.swift
+//  GitHubJournal
+//
+//  Created by Nathan Andrus on 9/4/19.
+//  Copyright © 2019 Nathan Andrus. All rights reserved.
+//
+
+import UIKit
+
+class JournalTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    // MARK: - Table view data source
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+
+        // Configure the cell...
+
+        return cell
+    }
+ 
+    // MARK: - Navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+    }
+}
