0be5b5521a15fbbc68b03f8bfb63b5d1848ed7d0
Nathan Andrus <nathanpandrus@gmail.com>
2019-09-04 11:33:33 -0600

    change cell text :wq! i

--- GitHubJournal/JournalTableViewController.swift
+++ GitHubJournal/JournalTableViewController.swift
@@ -35,6 +35,7 @@ class JournalTableViewController: UITableViewController {
         cell.textLabel?.text = "yessir"
         cell.detailTextLabel?.text = "ooooohhhhhhhhh"
         cell.backgroundColor = .green
+        cell.textLabel?.text = "Ian is cooler than Jax X100"
 
         return cell
     }
